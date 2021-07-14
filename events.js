a67259ad4119255c9896fffbb3fa040b9a47f0e6
Nafas <n.dosarian@gmail.com>
2021-07-14 18:42:14 +0430

    Events: incidents ajax load

--- events.js
+++ events.js
@@ -1,12 +1,13 @@
 events={
     welcome:null,
+    container:null,
+    mainclonable:null,
+    res:null,
     init:()=>{
         events.welcome = document.getElementById("profile-txt")
         login.loading = document.getElementById("loading");
-        setTimeout(() => {
-           login.loading.classList.add("d-none");
-        
-       }, 300);
+        events.container=document.getElementsByClassName("container")[0];
+        events.mainclonable = document.getElementsByClassName('main')[0];
        events.checklogin()
     },
     checklogin:()=>{
@@ -16,7 +17,40 @@ events={
             }, 300);
         } else {
             events.setUsername()
+            events.loadEvents()
         }
+    },
+    setFeild:(elem,fild,value)=>{
+        elem.getElementsByClassName(fild)[0].innerHTML=value
+    },
+    showEvents:(data)=>{
+        data.forEach(element => {
+            let neew = events.mainclonable.cloneNode(true);
+            neew.classList.remove("d-none");
+            events.setFeild(neew,"botnet-name",element.incident_type)
+            events.setFeild(neew,"botnet-id","..."+"#" + element.incident_id.substring(0,7))
+            events.setFeild(neew,"siem-btn",element.siem.name)
+            events.setFeild(neew,"time-show",moment(element.reported_time).fromNow())
+            events.setFeild(neew,"p1",element.sources[0].ips[0].ip)
+            events.setFeild(neew,"p2",element.destinations[0].ips[0].ip)
+            events.setFeild(neew,"port",element.sources[0].services[0].port)
+            
+            events.container.appendChild(neew); 
+        });
+    },
+    loadEvents:()=>{
+        const xhttp = new XMLHttpRequest();
+        xhttp.onload = function() {
+            events.res = JSON.parse(this.responseText)
+            events.showEvents(events.res.results);
+            setTimeout(() => {
+                login.loading.classList.add("d-none");
+             
+            }, 300);
+            
+        }
+        xhttp.open("GET", "incidents", true);
+        xhttp.send();
     },
     setUsername:()=>{
         let user = login.getuser().split("@")
